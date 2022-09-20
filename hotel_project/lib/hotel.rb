66a3416e0007c3340f1cca3ca6a8ad1cf3cb07b9
lorenzo <lorenzotijerina@gmail.com>
2022-09-20 19:51:39 -0400

    pass hotel spec up to check in

--- hotel_project/lib/hotel.rb
+++ hotel_project/lib/hotel.rb
@@ -1,5 +1,34 @@
+require "byebug"
 require_relative "room"
 
 class Hotel
-  
+
+    def initialize(name, room_hash)
+        @name = name
+        @rooms = {}
+        room_hash.each do |name,beds|
+            @rooms[name] = Room.new(beds)
+        end
+
+    end
+
+    def name
+        # debugger
+        (@name.split(" ").map {|n| n.capitalize}).join(" ")
+    end
+
+    def rooms
+        @rooms
+    end
+
+    def room_exists?(room)
+        rooms.include?(room)
+    end
+
+    def check_in(person, room)
+        if !room_exists?(room)
+            puts "sorry, room does not exist"
+            
+    end
+
 end
