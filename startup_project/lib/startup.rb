323cb2282198d2a386a8328830082492f628be4e
lorenzo <lorenzotijerina@gmail.com>
2022-09-20 23:42:11 -0400

    must sleep

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -1,5 +1,16 @@
 require "employee"
-
+attr_reader :name,:funding,:salaries,:employees
 class Startup
+    def initialize(name, funding, salaries)
+        @name = name 
+        @funding = funding
+        @salaries = salaries
+        @employees = []
+    end
+
+
+
+
 
 end
+
